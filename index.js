9c1267f1536ea11c85fd06803d6f456e3165bd50
sandybrowne <110634886+sandybrowne@users.noreply.github.com>
2023-07-13 20:40:08 +0100

    commit inc random images change

---
+++ index.js
@@ -0,0 +1,44 @@
+const randomNumber1 = Math.floor((Math.random() * 6) + 1);
+const randomNumber2 = Math.floor((Math.random() * 6) + 1);
+
+console.log(randomNumber1);
+console.log(randomNumber2);
+// const RandomNumberAttribute 
+
+const diceImage1 = () => {
+    if (randomNumber1 == 1) {
+        document.querySelector("img.img1").setAttribute("src", "./images/dice1.png");
+    }
+    else if (randomNumber1 == 2) {
+        document.querySelector("img.img1").setAttribute("src", "./images/dice2.png");
+    }
+    else if (randomNumber1 == 3) {
+        document.querySelector("img.img1").setAttribute("src", "./images/dice3.png");
+    }
+    else if (randomNumber1 == 4) {
+        document.querySelector("img.img1").setAttribute("src", "./images/dice4.png");
+    }
+    else if (randomNumber1 === 5) {
+        document.querySelector("img.img1").setAttribute("src", "./images/dice5.png");
+    }
+}
+diceImage1();
+
+const diceImage2 = () => {
+    if (randomNumber2 == 1) {
+        document.querySelector("img.img2").setAttribute("src", "./images/dice1.png");
+    }
+    else if (randomNumber2 == 2) {
+        document.querySelector("img.img2").setAttribute("src", "./images/dice2.png");
+    }
+    else if (randomNumber2 == 3) {
+        document.querySelector("img.img2").setAttribute("src", "./images/dice3.png");
+    }
+    else if (randomNumber2 == 4) {
+        document.querySelector("img.img2").setAttribute("src", "./images/dice4.png");
+    }
+    else if (randomNumber2 === 5) {
+        document.querySelector("img.img2").setAttribute("src", "./images/dice5.png");
+    }
+}
+diceImage2();
